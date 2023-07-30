1ddb58187246871b4df5f2dd82d26594debd1d5d
MitkoDG <retimid@abv.bg>
2023-07-30 23:30:04 +0300

    adding useEffect to load the excel file on page load

--- src/components/ExcelInput.js
+++ src/components/ExcelInput.js
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
 
 export const ExcelInput = () => {
 
     const [data, setData] = useState([]);
+    const filePath = "../../excel_files/playtv_plans_all.xlsx"
+    useEffect(() => {
+        // Load the data from the Excel file when the component mounts
+        const fetchExcelData = async () => {
+            try {
+                const response = await fetch(filePath);
+                const blob = await response.blob();
+                const reader = new FileReader();
+                reader.readAsBinaryString(blob);
+                reader.onload = (e) => {
+                    const data = e.target.result;
+                    const workbook = XLSX.read(data, { type: "binary" });
+                    const sheetName = workbook.SheetNames[0];
+                    const sheet = workbook.Sheets[sheetName];
+                    const parsedData = XLSX.utils.sheet_to_json(sheet);
+                    setData(parsedData);
+                };
+            } catch (error) {
+                console.error("Error loading Excel file:", error);
+            }
+        };
 
+        fetchExcelData();
+    }, []);
     const handleFileUpload = (e) => {
         const reader = new FileReader();
         reader.readAsBinaryString(e.target.files[0]);
