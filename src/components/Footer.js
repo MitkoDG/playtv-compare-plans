812531dec7e40ad8a6f69bc8c3e4ed2abbd8c5c4
MitkoDG <retimid@abv.bg>
2023-08-31 22:46:22 +0300

    fixing the style and updating the channel list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className="main-footer">
             <nav className="nav-footer">
                 <a href="https://dimitargegov.site/" alt="Logo"><img src={logo} alt="Logo" /></a>
-                <li className="list-footer">Last updated: 04/08/2023</li>
+                <li className="list-footer">Last updated: 31/08/2023</li>
                 <li className="list-footer"><li className="howitworks">How it works ?</li></li>
                 <li className="list-footer"><a href="https://playtv.bg/%d0%b0%d0%b1%d0%be%d0%bd%d0%b0%d0%bc%d0%b5%d0%bd%d1%82%d0%b8-4/" target="blank" rel="noopener noreferrer">Official Website</a></li>
             </nav>
