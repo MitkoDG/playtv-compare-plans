f862292489e247baef451529f9ad8e242c42e9f3
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2023-09-04 15:34:50 +0300

    fixing the error in the console

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
-import logo from "../assets/images/ddg-style.project.jpg";
+import logo from '../assets/images/ddg-style.project.jpg'
 
 export const Footer = () => {
-    return (
-        <footer className="main-footer">
-            <nav className="nav-footer">
-                <a href="https://dimitargegov.site/" alt="Logo"><img src={logo} alt="Logo" /></a>
-                <li className="list-footer">Last updated: 31/08/2023</li>
-                <li className="list-footer"><li className="howitworks">How it works ?</li></li>
-                <li className="list-footer"><a href="https://playtv.bg/%d0%b0%d0%b1%d0%be%d0%bd%d0%b0%d0%bc%d0%b5%d0%bd%d1%82%d0%b8-4/" target="blank" rel="noopener noreferrer">Official Website</a></li>
-            </nav>
-        </footer>
-    )
-};
+  return (
+    <footer className="main-footer">
+      <nav className="nav-footer">
+        <a href="https://dimitargegov.site/" alt="Logo">
+          <img src={logo} alt="Logo" />
+        </a>
+        <ul>
+          <li className="list-footer">Last updated: 31/08/2023</li>
+          <li className="list-footer">
+            <li className="howitworks">How it works ?</li>
+          </li>
+          <li className="list-footer">
+            <a
+              href="https://playtv.bg/%d0%b0%d0%b1%d0%be%d0%bd%d0%b0%d0%bc%d0%b5%d0%bd%d1%82%d0%b8-4/"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              Official Website
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
