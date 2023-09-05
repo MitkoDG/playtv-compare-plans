a05e914da0e86b8d75baf8146b387cc677cbac34
MitkoDG <retimid@abv.bg>
2023-09-05 20:55:04 +0300

    added new logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import logo from '../assets/images/ddg-style.project.jpg'
+import logoNoBackground from "../assets/images/logo.png"
 
 export const Footer = () => {
   return (
     <footer className="main-footer">
       <nav className="nav-footer">
         <a href="https://dimitargegov.site/" alt="Logo">
-          <img src={logo} alt="Logo" />
+          <img src={logoNoBackground} alt="Logo" />
         </a>
           <li className="list-footer">Last updated: 31/08/2023</li>
           <li className="list-footer">
@@ -14,7 +15,7 @@ export const Footer = () => {
           <li className="list-footer">
             <a
               href="https://playtv.bg/%d0%b0%d0%b1%d0%be%d0%bd%d0%b0%d0%bc%d0%b5%d0%bd%d1%82%d0%b8-4/"
-              target="blank"
+              target="_blank"
               rel="noopener noreferrer"
             >
               Official Website
